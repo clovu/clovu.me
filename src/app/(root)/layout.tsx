7ed9c105c3afd554210ad9dc45bf17f391a72eba
Clover You <hi@clovu.me>
2026-05-02 09:08:51 +0800

    feat: add GoTopBtn component for smooth scrolling to top

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,8 +1,11 @@
+'use client'
+import { GoTopBtn } from '@/components/gotop'
 import { Header } from '@/components/header'
 
 export default function PostsLayout({ children }: React.PropsWithChildren) {
   return <>
     <Header />
+    <GoTopBtn />
     {children}
   </>
 }
---
+++ src/components/gotop.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { useWindowScroll } from 'react-use'
+import { Button } from './ui/button'
+
+export function GoTopBtn() {
+  function toTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  const windowScroll = useWindowScroll()
+  const showClass = windowScroll.y > 300 ? 'opacity-30' : '!opacity-0 pointer-events-none'
+
+  return (
+    <Button
+      size="icon"
+      className={`fixed w-10 h-10 right-3 bottom-20 hover:opacity-100 ${showClass} z-10 `}
+      variant="ghost"
+      onClick={toTop}
+      aria-label="Go to the top"
+    >
+      <span className="icon-[humbleicons--arrow-up] w-4 h-4"></span>
+    </Button>
+  )
+}
